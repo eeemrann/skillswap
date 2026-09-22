2b82219d1d7f544b4c6da802f522fd5e8139cee5
eeemrann <ahmedemran412@gmail.com>
2026-09-23 01:52:50 +0600

    fixed all issues

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { googleLogin, logout } = require('../controllers/authController');
+const { logout } = require('../controllers/authController');
 const auth = require('../middleware/authMiddleware');
 
-router.post('/google', googleLogin);
 router.post('/logout', auth, logout);
 
 module.exports = router;
